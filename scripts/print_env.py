9c2b27f3080465d629c71c58c6e100c2fef1997c
John Carlyle-Clarke <john.carlyleclarke@deliveroo.co.uk>
2019-09-05 15:34:26 +0100

    SRE-xxx: Refactor printenv script (#43)
    Better error messages.
    Ensure errors sent to STDERR.

--- scripts/print_env.py
+++ scripts/print_env.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import os
 import sys
 
 
-def helpMessage():
-    print('Expected usage: `print_env $name`. Must be a string at least 1 character in length.')
+def help_message(error):
+    if error:
+        print(error, file=sys.stderr)
 
-def main():
-    args = sys.argv
+    print(
+        """
+Usage: print_env <target>
 
-    if len(args) != 2:
-        helpMessage()
-        sys.exit(1)
+    <target> - the variable prefix to match
 
-    target = args[1]
+    Example: a target of FOO will match all variables starting FOO_
+             such as FOO_TEST1 and FOO_TEST2
+""",
+        file=sys.stderr,
+    )
 
-    if not len(target):
-        helpMessage()
-        sys.exit(1)
 
-    prefix = '%s_' % target
+def print_env(target):
+
+    prefix = "%s_" % target
 
     for name, value in os.environ.items():
         if not name.startswith(prefix):
             continue
-        new_name = name[len(prefix):]
-        print 'export %s=%s' % (new_name, value)
+        new_name = name.lstrip(prefix)
+        print("export %s=%s" % (new_name, value))
 
 
-if __name__ == '__main__':
-    main()
+def main(args):
+    if len(args) != 1:
+        help_message("Exactly one argument required")
+        sys.exit(1)
+
+    target = args[0]
+
+    if not target:
+        help_message("Prefix cannot be empty")
+        sys.exit(1)
+
+    print_env(target)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
